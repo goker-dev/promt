84fbf08f8db2b728b82038369129007e21816552
goker <goker@cebeci.name>
2014-05-07 21:08:45 +0300

    chat

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
 var express = require('express'),
     app = express(), // Web framework to handle routing requests
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
     cons = require('consolidate'), // Templating library adapter for Express
     swig = require('swig'), // Templating library adapter for Express
     MongoClient = require('mongodb').MongoClient, // Driver for connecting to MongoDB
     routes = require('./routes'); // Routes for our application
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+// routing
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// usernames which are currently connected to the chat
+var usernames = {};
+
+// rooms which are currently available in chat
+var rooms = ['design','development','general'];
+
+io.sockets.on('connection', function (socket) {
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username){
+		// store the username in the socket session for this client
+		socket.username = username;
+		// store the room name in the socket session for this client
+		socket.room = 'general';
+		// add the client's username to the global list
+		usernames[username] = username;
+		// send client to room 1
+		socket.join('general');
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected to general');
+		// echo to room 1 that a person has connected to their room
+		socket.broadcast.to('general').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.emit('updaterooms', rooms, 'general');
+	});
+
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function (data) {
+		// we tell the client to execute 'updatechat' with 2 parameters
+		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+	});
+
+	socket.on('switchRoom', function(newroom){
+		// leave the current room (stored in session)
+		socket.leave(socket.room);
+		// join new room, received as function parameter
+		socket.join(newroom);
+		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+		// sent message to OLD room
+		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+		// update socket session room title
+		socket.room = newroom;
+		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+		socket.emit('updaterooms', rooms, newroom);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.leave(socket.room);
+	});
+});
 
 MongoClient.connect('mongodb://localhost:27017/promt', function (err, db) {
     "use strict";
@@ -46,6 +117,6 @@ MongoClient.connect('mongodb://localhost:27017/promt', function (err, db) {
     app.use(connect.bodyParser());
     // Application routes
     routes(app, db);
-    app.listen(8082);
+    server.listen(8082);
     console.log('Express server listening on port 8082');
 });
